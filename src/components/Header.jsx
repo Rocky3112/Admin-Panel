c613fb12e6db15e6aa198a65a25534cedf7d829e
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-11-19 17:01:09 +0600

    added some icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify }
+    from 'react-icons/bs'
 
 const Header = () => {
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <header className='header'>
+            <div className='menu-icon'>
+                {/* <BsJustify className='icon' onClick={OpenSidebar} /> */}
+            </div>
+            <div className='header-left'>
+                <BsSearch className='icon' />
+            </div>
+            <div className='header-right'>
+                <BsFillBellFill className='icon' />
+                <BsFillEnvelopeFill className='icon' />
+                <BsPersonCircle className='icon' />
+            </div>
+        </header>
     );
 };
 
