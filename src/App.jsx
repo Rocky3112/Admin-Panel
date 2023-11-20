b6a9b926a5ae59e99a63a93f24dce52832be2987
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-11-20 23:01:48 +0600

    dashboard completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
@@ -6,14 +7,20 @@ import SideBar from './components/SideBar'
 
 function App() {
 
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
 
   return (
-   <div className='grid-container'>
-        <Header></Header>
-        <SideBar></SideBar>
-        <Home></Home>
-   </div>
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <SideBar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
   )
 }
+
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify }
     from 'react-icons/bs'
 
-const Header = () => {
+const Header = ({OpenSidebar}) => {
     return (
         <header className='header'>
             <div className='menu-icon'>
-                {/* <BsJustify className='icon' onClick={OpenSidebar} /> */}
+                <BsJustify className='icon' onClick={OpenSidebar} />
             </div>
             <div className='header-left'>
                 <BsSearch className='icon' />
